fbea122a55f3e015179c0d58def413ff4f39e1ce
Baizak31052004 <nabaizak173@gmail.com>
2023-04-03 13:49:42 +0600

    game

--- class-day1.py
+++ class-day1.py
@@ -54,41 +54,39 @@
 # Inheritance
 
 
-class Car():
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 100
+# class Car():
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 100
 
-    def get_descriptive_name(self):
-        long_name = f"{self.year} {self.make} {self.model}"
-        return long_name.title()
+#     def get_descriptive_name(self):
+#         long_name = f"{self.year} {self.make} {self.model}"
+#         return long_name.title()
     
-    def read_descriptive_name(self):
-        print(f"This car has {self.odometer_reading} miles on it.")
+#     def read_descriptive_name(self):
+#         print(f"This car has {self.odometer_reading} miles on it.")
 
-    def update_odometer(self, mileage):
-        if mileage >= self.odometer_reading:
-            self.odometer_reading = mileage
-        else:
-            print("You can't roll back an odometer!")
+#     def update_odometer(self, mileage):
+#         if mileage >= self.odometer_reading:
+#             self.odometer_reading = mileage
+#         else:
+#             print("You can't roll back an odometer!")
 
-    def increment_odometer(self, miles):
-        self.odometer_reading += miles
+#     def increment_odometer(self, miles):
+#         self.odometer_reading += miles
 
-class ElectricCar(Car):
-    def __init__(self, make, model, year):
-        super().__init__(make, model, year)
-        self.battery_size = 80
+# class ElectricCar(Car):
+#     def __init__(self, make, model, year):
+#         super().__init__(make, model, year)
+#         self.battery_size = 80
     
-    def descrip_battery(self):
-        print(self.battery_size)
+#     def descrip_battery(self):
+#         print(self.battery_size)
 
-my_tesla = ElectricCar('tesla', 'model s', 2022)
-print(my_tesla.get_descriptive_name())
-my_tesla.descrip_battery()
+# my_tesla = ElectricCar('tesla', 'model s', 2022)
+# print(my_tesla.get_descriptive_name())
+# my_tesla.descrip_battery()
 
 
- git config --global user.name "Baizak31052004"
- git config --global user.email nabaizak173@gmail.com
